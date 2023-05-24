e90d065452783b44f089f435b3a8d502df0340e1
OP-Oscar <oscarpena07@outlook.com>
2023-05-24 10:22:21 -0500

    section sample added

--- public/fscript/main.js
+++ public/fscript/main.js
@@ -12,3 +12,40 @@ console.log(currentHTMLFile);
 //event listeners
 
 
+// // Get user's location using Geolocation API
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+//   } else {
+//     console.log('Geolocation is not supported by this browser.');
+//   }
+  
+//   // Success callback function for Geolocation API
+//   function successCallback(position) {
+//     var latitude = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+  
+//     // Call weather API using the retrieved coordinates
+//     getWeatherForecast(latitude, longitude);
+//   }
+  
+//   // Error callback function for Geolocation API
+//   function errorCallback(error) {
+//     console.log('Error occurred while retrieving location:', error.message);
+//   }
+  
+//   // Function to fetch weather forecast using weather.gov API
+//   function getWeatherForecast(latitude, longitude) {
+//     var apiUrl = 'https://api.weather.gov/points/' + latitude + ',' + longitude + '/forecast';
+  
+//     fetch(apiUrl)
+//       .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(data) {
+//         // Process the weather forecast data
+//         console.log(data);
+//       })
+//       .catch(function(error) {
+//         console.log('Error:', error);
+//       });
+//   }
