9a87cf2d0b60ab265349674c696702b04b41a97c
OP-Oscar <oscarpena07@outlook.com>
2023-05-26 06:56:14 -0500

    deploy ready

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,7 @@ const {CONNECTION_STRING}= process.env;
 
 const {Sequelize} = require('sequelize');
 
+
 const sequelize = new Sequelize(CONNECTION_STRING, {
     dialect: 'postgres',
     dialectOptions: {
